0e126590a852efe15bae9ec811d6b2708eb112f0
Jeffry Samuel Eduarte Rojas <jeffrysamuer@gmail.com>
2024-01-24 18:33:49 -0600

    Creacion de proyecto

---
+++ tienda_Jeffry_Eduarte_Rojas/src/main/java/tienda/demo/TiendaJeffryEduarteRojasApplication.java
@@ -0,0 +1,13 @@
+package tienda.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaJeffryEduarteRojasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaJeffryEduarteRojasApplication.class, args);
+	}
+
+}
