fcab34c785663cc97c6e76acae0d5338bdd3deaa
Jeffser <jeffrysamuer@gmail.com>
2024-04-10 20:22:18 -0600

    Semana 13

---
+++ tienda_Jeffry_Eduarte_Rojas/src/main/java/tienda/demo/service/ItemService.java
@@ -0,0 +1,18 @@
+package tienda.demo.service;
+
+import tienda.demo.domain.Item;
+import java.util.ArrayList;
+import java.util.List;
+
+public interface ItemService {
+    List<Item> listaItems = new ArrayList<>();
+    public List<Item> gets();
+
+    public Item get(Item item);
+
+    public void delete(Item item);
+
+    public void save(Item item);
+    public void actualiza(Item item);
+    public void facturar();
+}
